ef1597f66493c9e2e0ee691bb3f3806c780f963d
SalinaSth <salluz2014@gmail.com>
2021-07-06 20:26:54 +0545

    customer insert route added

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const path = require('path')
 const app = express()
 
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
+const db = require('./database/db')
+
+const route_customer = require('./route/route_customer')
+
+app.use(route_customer)
+app.listen(3000)
