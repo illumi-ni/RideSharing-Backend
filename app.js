6a2982f60fe9e048ff009d79ed743b890a8b6939
Reshika Shrestha <shresthareshika@gmail.com>
2021-07-07 11:35:34 +0545

    Driver registration done

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const path = require('path')
-const app = express()
 
+const app = express()
+app.use(cors())
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended : false}))
+
+const db = require('./database/db')
+const driver_route = require('./route/DriverRegister_Route')
+
+app.use(express.static("images"))
+app.use(driver_route)
+
+app.listen(90)
