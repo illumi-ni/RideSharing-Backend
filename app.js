e07c21fa5669994abff3dc387049c575a4f230e6
Meezu Lawot <omizuki123@gmail.com>
2021-07-08 16:27:32 +0545

    send otp in email

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ app.use(bodyParser.urlencoded({extended : false}))
 const db = require('./database/db')
 const driver_route = require('./route/DriverRegister_Route')
 const route_customer = require('./route/route_customer')
+const routeotp = require('./route/OTPverify')
 
 app.use(express.static("images"))
 app.use(driver_route)
-
 app.use(route_customer)
+app.use(routeotp)
 
 app.listen(90)
 
