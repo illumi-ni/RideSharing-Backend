d03ca5c876ee4363aef76ae73e47baad45220f5c
roy94576 <roy94576@gmail.com>
2021-07-14 13:30:37 +0545

    advanced booking route added

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ const driver_route = require('./route/DriverRegister_Route');
 const route_customer = require('./route/route_customer');
 const admin_route = require('./route/Admin_Route');
 const routeotp = require('./route/OTPverify');
+const Bookingadvance_route = require('./route/Bookingadvance_route')
 
 app.use(express.static("images"));
 app.use(driver_route);
 app.use(route_customer);
 app.use(admin_route)
 app.use(routeotp);
+app.use(Bookingadvance_route)
 
 const PORT=process.env.PORT||90;
 app.listen(PORT,()=>{
