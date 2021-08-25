e77ab9baf9be1b0ab1e103ae7517e9651b482311
SalinaSth <salluz2014@gmail.com>
2021-08-25 13:37:33 +0545

    minor changes

--- route/customer_route.js
+++ route/customer_route.js
@@ -154,14 +154,14 @@ router.put('/customer/update/:id', auth.checkCustomer, function (req, res) {
     const id = req.params.id
     const fullname = req.body.fullname;
     const email = req.body.email;
-    const phone = req.body.phone;
+    const contact = req.body.contact;
     const gender = req.body.gender;
     const photo = req.body.photo;
 
-    console.log(id, fullname, email, phone, gender)
+    console.log(id, fullname, email, contact, gender)
 
     Customer.updateOne({ _id: id }, {
-        fullname: fullname, email: email, phone: phone, gender: gender
+        fullname: fullname, email: email, contact: contact, gender: gender
     })
     .then(function (result) {
         res.status(200).json({ message: "Customer updated", success: true })
