21a4db229aee811358cce4c2d619f76ada613ff2
Meezu Lawot <omizuki123@gmail.com>
2021-08-26 22:00:18 +0545

    Driver profile updated and driver image uploaded

--- route/driver_route.js
+++ route/driver_route.js
@@ -19,7 +19,7 @@ router.post('/admin/register', upload.fields([{ name: 'photo', maxCount: 1 }, { 
     const phone = req.body.phone;
     const citizenship = req.body.citizenship;
     const licence = req.body.licence;
-    const dob = req.body.dob;
+    const gender = req.body.gender;
     const vechileNo = req.body.vechileNo;
     const model = req.body.model;
     const brand = req.body.brand;
@@ -31,7 +31,7 @@ router.post('/admin/register', upload.fields([{ name: 'photo', maxCount: 1 }, { 
     bcryptjs.hash(password, 10, function (err, hash) {
         const driverData = new Driver({
             fullname: fullname, email: email, username: username, password: hash,
-            phone: phone, citizenship: citizenship, licence:  req.files.licence[0].filename, dob: dob, vechileNo: vechileNo, model: model, brand:brand, photo:req.files.photo[0].filename
+            phone: phone, citizenship: citizenship, licence:  req.files.licence[0].filename, gender: gender, vechileNo: vechileNo, model: model, brand:brand, photo:req.files.photo[0].filename
         });
         driverData.save().then(function (result) {
             res.status(201).json({ success: true, message: "Driver Registration has been successfully inserted!!!" });
@@ -92,6 +92,45 @@ router.get('/driver/single', auth.checkDriver, function(req,res){
         .catch(function (e) {
             res.status(500).json({ message: e, success: false });
         })
+})
+
+router.put('/driver/update/:id', auth.checkDriver, function (req, res) {
+
+    const id = req.params.id;
+    const fullname = req.body.fullname;
+    const email = req.body.email;
+    const username = req.body.username;
+    const phone = req.body.phone;
+    const gender = req.body.gender;
+    const citizenship = req.body.citizenship;
+    const licence = req.body.licence;
+    const vechileNo = req.body.vechileNo;
+    const photo= req.body.photo;
+
+    Driver.updateOne({ _id: id }, { fullname: fullname, username: username, phone: phone, email: email, gender: gender, citizenship: citizenship, licence: licence, vechileNo: vechileNo })
+        .then(function (result) {
+            res.status(201).json({ messsage: "Driver updated!!", success: true })
+
+        })
+        .catch(function (err) {
+            res.status(500).json({ messsage: err, success: false })
+    })
+
+})
+
+router.put('/driver/updateImage', auth.checkDriver, upload.single('photo'), function (req, res) {
+    const id = req.driverData._id;
+    if (req.file == undefined) {
+        console.log(req.file);
+        return res.status(201).json({ success: false, message: "Invalid  file format" })
+    }
+    Driver.updateOne({ _id: id }, {
+        photo:req.file.filename
+    }).then(function (data) {
+        res.status(200).json({ message: 'image updated', success: true })
+    }).catch(function (err) {
+        res.send.status(500).json({ message: err , success: false})
+    });
 })
 
 router.delete('/delete/driver/:did', function (req, res) {
